1207621101fe9bacc0e6ca9e053c841bbc1fd557
LafryCZ <150274941+LafryCZ@users.noreply.github.com>
2024-02-12 18:27:36 +0100

    Add files via upload

---
+++ Lekce_1/A-Datove typy/c_operace_s_retezci_cviceni_hotovo.py
@@ -0,0 +1,18 @@
+################ Úkol 1 ################
+
+# Pomocí operace spojení (+) (konkatenace) spojte dohromady tyto tři řetězce:
+# "Dobrý den, " "vítejte na " "našem webu."
+
+print ("Dobrý den, " + "vítejte na " + "našem webu.")
+
+################ Úkol 2 ################
+
+# Pomocí indexování získejte z řetězce "Ahoj" znak "j".
+
+print("Ahoj"[-1])
+
+################ Úkol 3 ################
+
+# Pomocí slicingu získejte z řetězce "Hello world!" podřetězec "wor".
+
+print("Hello world!"[6:9]) 
